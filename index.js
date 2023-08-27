2acba8504bf878693217e3e7f7a6e57a9890fdea
Phantather <akdil.hstech@gmail.com>
2023-08-27 18:15:48 +0600

    add mongoose

--- index.js
+++ index.js
@@ -1,13 +1,20 @@
 import express from 'express'
-import {getAllUsers, getOneUser} from "./controller/users/users.js";
+import {createOneUser, getAllUsers, getOneUser, updateOneUser} from "./controller/users/users.js";
+import mongoose from "mongoose";
 
 const api = express()
+api.use(express.json())
+
+mongoose.connect('mongodb+srv://ouuakdilkg:Asd12345@euphoria.jqgojtg.mongodb.net/?retryWrites=true&w=majority')
+    .then(() => console.log('Mongo DB is Start'))
+    .catch((error) => console.log(error))
 
 const PORT = 8080
 
 api.get('/users', getAllUsers)
 api.get('/users/:id', getOneUser)
-
+api.post('/users', createOneUser)
+api.patch('/users/:id', updateOneUser)
 api.listen(8080, () => {
     console.log(`server is done on http://localhost:${PORT}`)
 })
